76b0095eed797ae8b7594b23cecc0f2155306c5c
rcheruiyot <rcheruiyot@zamara.co.ke>
2024-07-08 18:37:17 +0300

    Report page

---
+++ src/components/dashboard/reports/reportsModal.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  // border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ReportsModal({modal, setModal, name}: {modal: boolean, setModal: (boolean: boolean) => void, name: string}) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setModal(false)
+
+  return (
+    <>
+      {/* <Button onClick={handleOpen}>Open modal</Button> */}
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={modal}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={modal}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              {name}
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Get {name} Report
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </>
+  );
+}
